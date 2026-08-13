0010e7f9abc7b884e33b38e350af710f4b210d11
Santatriniaina Rahantanirina <hei.santatriniaina.2@gmail.com>
2026-08-13 15:04:02 +0300

    Modifications of post

--- server.js
+++ server.js
@@ -24,86 +24,70 @@ app.get("/students", (request, response) => {
 
 app.get("/students/:id", (request, response) => {
     const idStudent = parseInt(request.params.id);
-
     const student = students.find(student => student.id === idStudent);
-
     if (!student) {
         return response.status(404).json({
             message: "Student not found"
         });
     }
-
     response.status(200).json(student);
 });
 
 
+
 app.post("/students", (request, response) => {
-    const newStudent = {
-        id: students.length > 0
-            ? Math.max(...students.map(student => student.id)) + 1
-            : 1,
+    let newStudent = {
+        id: students.length + 1,
         name: request.body.name,
         age: request.body.age
     };
-
     students.push(newStudent);
-
     response.status(201).json(newStudent);
 });
 
 
+
 app.put("/students/:id", (request, response) => {
     const idStudent = parseInt(request.params.id);
-
     const i = students.findIndex(student => student.id === idStudent);
-
     if (i === -1) {
         return response.status(404).json({
             message: "Student not found"
         });
     }
-
     students[i].name = request.body.name;
     students[i].age = request.body.age;
-
     response.status(200).json(students[i]);
 });
 
 
+
 app.patch("/students/:id", (request, response) => {
     const idStudent = parseInt(request.params.id);
-
     const student = students.find(student => student.id === idStudent);
-
     if (!student) {
         return response.status(404).json({
             message: "Student not found"
         });
     }
-
     if (request.body.name !== undefined) {
         student.name = request.body.name;
     }
-
     if (request.body.age !== undefined) {
         student.age = request.body.age;
     }
-
     response.status(200).json(student);
 });
 
 
 app.delete("/students/:id", (request, response) => {
     const idStudent = parseInt(request.params.id);
-
-    const index = students.findIndex(student => student.id === idStudent);
-
-    if (index === -1) {
+    const i = students.findIndex(student => student.id === idStudent);
+    if (i === -1) {
         return response.status(404).json({
             message: "Student not found"
         });
     }
-
     const deletedStudent = students.splice(index, 1);
 
     response.status(200).json({
